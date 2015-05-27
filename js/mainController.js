8e792560b9ba943ab1d07af206009ab99cc8ecbe
Arturo <arturo.romerotcs@gmail.com>
2015-05-26 20:50:37 -0600

    finished UI

--- js/mainController.js
+++ js/mainController.js
@@ -1,9 +1,20 @@
 angular
 	.module('SoundCloudApp')
-	.controller('MainController', function($scope, $log, MainService) {
-		$scope.getUser = function(userName) {
-			MainService.getUsers(userName);
+	.controller('MainController', function($scope, $log, $sce, MainService) {
+		$scope.searchText = '';
+
+		$scope.getUser = function(user) { 
+			$scope.searchText = '';
+			MainService.getUsers(user)
+				.then(function(user) {
+					$scope.userData = user;
+				});
 		}
 
-		$scope.getUser('lifeplus')
+		$scope.play = function(track_url) {
+			SC.oEmbed(track_url, { auto_play: true }, function(oEmbed) {
+	      $scope.$apply($scope.player_html = $sce.trustAsHtml(oEmbed.html));
+	    });
+		}
+		
 	})
--- js/mainService.js
+++ js/mainService.js
@@ -2,11 +2,12 @@ angular
 	.module('SoundCloudApp')
 	.service('MainService', function($http, $log) {
 		
-		this.getUsers = function(userName) {
-			$http.get('http://api.soundcloud.com/users/' + userName + '/tracks.json?client_id=bda4ada8694db06efcac9cf97b872b3e')
+		this.getUsers = function(userName) { 
+			return $http.get('http://api.soundcloud.com/users/' + userName + '/tracks.json?client_id=bda4ada8694db06efcac9cf97b872b3e')
 				.then(function(res) {
 					var data = res.data;
-					console.log(data);
+					console.log(data)
+					return data;
 				});
 			}
 	})
