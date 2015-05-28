e1540fce955f9298a10f1e39e48db29a9fe013c1
Arturo <arturo.romerotcs@gmail.com>
2015-05-28 17:29:58 -0600

    refactor code

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
 angular
-	.module('SoundCloudApp', [])
+	.module('SoundCloudApp', ['ngRoute'])
+	.config(function($routeProvider, $httpProvider) {
+		// $httpProvider.interceptors.push('httpRequestInterceptor');
+			$routeProvider
+				.when('/', {
+					templateUrl: 'pages/tracks.html',
+					controller: 'TracksController'
+				});
+	})
--- js/mainController.js
+++ js/mainController.js
@@ -1,10 +1,12 @@
 angular
 	.module('SoundCloudApp')
-	.controller('MainController', function($scope, $log, $sce, MainService) {
+	.controller('MainController', function($scope, $log, $sce, $routeParams, MainService) {
 		$scope.searchText = '';
 
 		$scope.getUser = function(user) { 
 			$scope.searchText = '';
+			$routeParams.userId = user;
+			console.log($routeParams.userId);
 			MainService.getUsers(user)
 				.then(function(user) {
 					$scope.userData = user;
